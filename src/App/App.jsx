895f6aea6feee57a5dbf98428b6fc59616be8552
klequis <klequis@gmail.com>
2018-10-31 21:37:02 -0700

    up

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,30 +1,13 @@
 import React, { Component } from 'react'
-import withRoot from '../withRoot'
-import AppBar from '../AppBar'
-import { Typography } from '@material-ui/core'
-import events from '../events'
-import EventGrid from '../EventGrid'
-import EventList from '../EventList'
-import EventSelect from '../EventSelect'
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <AppBar />
-        <Typography
-          variant='h4'
-          align='center'
-        >
-          Learn Material UI
-        </Typography>
-        <EventSelect events={events} />
-        <EventList events={events} />
-        <EventGrid events={events} />
-
+        <h1>Learn Material UI</h1>
       </div>
     )
   }
 }
 
-export default withRoot(App)
+export default App
